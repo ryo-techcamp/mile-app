b32bee4112dab68e760d751b2a27a3c2886bbe8d
ryo-techcamp <hurleyx.hp.81rt@outlook.jp>
2020-09-15 22:46:02 +0900

    投稿案件一覧表示実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,20 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path ,notice: '記事が投稿されました'
+    else
+      render :new
+    end  
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:image, :name, :description, :price)
+  end
+
 end
--- db/migrate/20200915042702_create_tasks.rb
+++ db/migrate/20200915042702_create_tasks.rb
@@ -1,7 +1,6 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-      t.text :image
       t.string :name
       t.text :description
       t.integer :price
