8422d5622a86c59c770c7d1a79d79b84bb30153f
ryo-techcamp <hurleyx.hp.81rt@outlook.jp>
2020-09-17 10:52:38 +0900

    ユーザー登録機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   def index
     @tasks = Task.all
   end
@@ -20,6 +21,12 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:image, :name, :description, :price)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "tasks#index"
   resources :tasks, only: [:index, :new, :create]
 end
