a5883d7bee5f1071bbd3b1ea308ca06bc23fb1f4
ryo-techcamp <hurleyx.hp.81rt@outlook.jp>
2020-09-17 15:39:03 +0900

    お問い合わせフォーム実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tasks#index"
-  resources :tasks, only: [:index, :new, :create,:show,:destroy,:edit,:update]
+  resources :tasks, only: [:index, :new, :create,:show,:destroy,:edit,:update] 
+  resources :contacts, only: [:index]
 end
