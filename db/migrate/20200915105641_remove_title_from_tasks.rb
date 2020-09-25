fc9e2dc6a8605f017074cf4edef5f71885efa94c
ryo-techcamp <hurleyx.hp.81rt@outlook.jp>
2020-09-25 15:01:01 +0900

    デプロイ前編集６

--- db/migrate/20200915105641_remove_title_from_tasks.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTitleFromTasks < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :tasks, :image, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_030851) do
+ActiveRecord::Schema.define(version: 2020_09_21_145634) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -45,10 +45,9 @@ ActiveRecord::Schema.define(version: 2020_09_22_030851) do
     t.string "name"
     t.text "description"
     t.integer "price"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.integer "mileage"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
